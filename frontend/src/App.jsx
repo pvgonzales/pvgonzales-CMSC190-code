327c9716d001110f5c82bbf1568d3cb312b9e0e7
pvgonzales <pvgonzales@up.edu.ph>
2026-03-23 20:08:26 +0800

    chore: added Analyze Page for model testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
+import AnalyzePage from './components/AnalyzePage';
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/analyze" element={<AnalyzePage />} />
       </Routes>
     </div>
   );
